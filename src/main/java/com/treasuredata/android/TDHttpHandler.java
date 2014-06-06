8882638854bf99a49ff904b892159247c0290327
Mitsunori Komatsu <komamitsu@gmail.com>
2014-06-06 20:13:44 +0900

    Fix java.security.cert.CertPathValidatorException when using HTTPS

--- src/main/java/com/treasuredata/android/TDHttpHandler.java
+++ src/main/java/com/treasuredata/android/TDHttpHandler.java
@@ -1,19 +1,31 @@
 package com.treasuredata.android;
 
 import io.keen.client.java.http.Request;
+import io.keen.client.java.http.Response;
 import io.keen.client.java.http.UrlConnectionHttpHandler;
 import org.komamitsu.android.util.Log;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 
 class TDHttpHandler extends UrlConnectionHttpHandler {
     private static final int DEFAULT_CONNECT_TIMEOUT = 30000;
     private static final int DEFAULT_READ_TIMEOUT = 30000;
     private static final String TAG = TDHttpHandler.class.getSimpleName();
-    private static final String DEFAULT_API_ENDPOINT = "http://in-staging.treasuredata.com/android/v3/event";
+    private static final String DEFAULT_API_ENDPOINT = "https://in.treasuredata.com/android/v3/event";
 
+    private final SSLContext sslContext;
     private final String apiKey;
     private final String apiEndpoint;
 
@@ -26,12 +38,32 @@ class TDHttpHandler extends UrlConnectionHttpHandler {
         }
         this.apiKey = apiKey;
         this.apiEndpoint = apiEndpoint;
+
+        SSLContext sslContext = null;
+        String errorLabel = "createSSLContext error: ";
+        try {
+            sslContext = createSSLContext();
+        } catch (CertificateException e) {
+            Log.e(TAG, errorLabel + e.getMessage());
+        } catch (KeyStoreException e) {
+            Log.e(TAG, errorLabel + e.getMessage());
+        } catch (NoSuchAlgorithmException e) {
+            Log.e(TAG, errorLabel + e.getMessage());
+        } catch (KeyManagementException e) {
+            Log.e(TAG, errorLabel + e.getMessage());
+        } catch (IOException e) {
+            Log.e(TAG, errorLabel + e.getMessage());
+        }
+        this.sslContext = sslContext;
     }
 
     protected HttpURLConnection openConnection(Request request) throws IOException {
-        // URL url = new URL("https://in.treasuredata.com/android/v3/event");
         URL url = new URL(this.apiEndpoint);
         HttpURLConnection result = (HttpURLConnection) url.openConnection();
+        if (url.getProtocol().equals("https")) {
+            HttpsURLConnection httpsURLConnection = (HttpsURLConnection) result;
+            httpsURLConnection.setSSLSocketFactory(sslContext.getSocketFactory());
+        }
         result.setConnectTimeout(DEFAULT_CONNECT_TIMEOUT);
         result.setReadTimeout(DEFAULT_READ_TIMEOUT);
         Log.d(TAG, "openConnection, request:" + request + ", result:" + result);
@@ -61,5 +93,40 @@ class TDHttpHandler extends UrlConnectionHttpHandler {
         connection.setDoOutput(true);
         request.body.writeTo(connection.getOutputStream());
         Log.d(TAG, "sendRequest, connection:" + connection + ", request:" + request);
+    }
+
+    @Override
+    protected Response readResponse(HttpURLConnection connection) throws IOException {
+        Response response = super.readResponse(connection);
+        Log.d(TAG, "readResponse: isSuccess=" + response.isSuccess());
+        return response;
+    }
+
+    private SSLContext createSSLContext() throws CertificateException, KeyStoreException, IOException, NoSuchAlgorithmException, KeyManagementException {
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        InputStream caInput = TDHttpHandler.class.getResourceAsStream("/gd_bundle.der");
+        java.security.cert.Certificate ca;
+        try {
+            ca = cf.generateCertificate(caInput);
+            // System.out.println("ca=" + ((X509Certificate) ca).getSubjectDN());
+        } finally {
+            caInput.close();
+        }
+        // Create a KeyStore containing our trusted CAs
+        String keyStoreType = KeyStore.getDefaultType();
+        KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+        keyStore.load(null, null);
+        keyStore.setCertificateEntry("ca", ca);
+
+        // Create a TrustManager that trusts the CAs in our KeyStore
+        String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+        tmf.init(keyStore);
+
+        // Create an SSLContext that uses our TrustManager
+        SSLContext context = SSLContext.getInstance("TLS");
+        context.init(null, tmf.getTrustManagers(), null);
+
+        return context;
     }
 }
