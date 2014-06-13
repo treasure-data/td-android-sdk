f717b3a73fc08772e22b1a64b5c15e0fa0fa9d86
Mitsunori Komatsu <komamitsu@gmail.com>
2014-06-13 21:03:02 +0900

    Refactor demo application

--- example/td-android-sdk-demo/src/com/treasuredata/android/demo/MainActivity.java
+++ example/td-android-sdk-demo/src/com/treasuredata/android/demo/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends Activity {
             v.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    final Map event = new HashMap<String, Object>(1);
+                    final Map event = new HashMap<String, Object>();
                     event.put("label", label);
                     event.put("id", v.getId());
                     event.put("left", v.getLeft());
