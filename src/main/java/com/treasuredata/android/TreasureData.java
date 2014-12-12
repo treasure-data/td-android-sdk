7b2dfb8fd10946c18aaeb28f70c66a9ed4921c28
Mitsunori Komatsu <komamitsu@gmail.com>
2014-12-12 22:12:51 +0900

    Update the version of User-Agent

--- src/main/java/com/treasuredata/android/TreasureData.java
+++ src/main/java/com/treasuredata/android/TreasureData.java
@@ -15,7 +15,7 @@ import java.util.regex.Pattern;
 
 public class TreasureData {
     private static final String TAG = TreasureData.class.getSimpleName();
-    private static final String VERSION = "0.1.5";
+    private static final String VERSION = "0.1.6";
     private static final String LABEL_ADD_EVENT = "addEvent";
     private static final String LABEL_UPLOAD_EVENTS = "uploadEvents";
     private static final Pattern DATABASE_NAME_PATTERN = Pattern.compile("^[0-9a-z_]{3,255}$");
