96c68b306e806705c89e50252dd3c60f1af9f22d
Mitsunori Komatsu <komamitsu@gmail.com>
2015-11-26 17:33:23 +0900

    Replace `Jackson` with `Gson`

--- src/main/java/com/treasuredata/android/TDJsonHandler.java
+++ src/main/java/com/treasuredata/android/TDJsonHandler.java
@@ -1,10 +1,13 @@
 package com.treasuredata.android;
 
 import android.util.Base64;
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.type.MapType;
-import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.internal.bind.TypeAdapters;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 import io.keen.client.java.KeenJsonHandler;
 import org.komamitsu.android.util.Log;
 
@@ -13,16 +16,37 @@ import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
+import java.lang.reflect.Type;
+import java.nio.charset.Charset;
 import java.security.InvalidKeyException;
 import java.security.MessageDigest;
-import java.text.SimpleDateFormat;
 import java.util.Map;
-import java.util.TimeZone;
 
 class TDJsonHandler implements KeenJsonHandler {
     private static final String TAG = TDJsonHandler.class.getSimpleName();
+    private static final Base64Encoder DEFAULT_BASE64_ENCODER = new Base64Encoder() {
+        @Override
+        public String encode(byte[] data)
+        {
+            return Base64.encodeToString(data, Base64.DEFAULT);
+        }
+
+        @Override
+        public byte[] decode(String encoded)
+        {
+            return Base64.decode(encoded, Base64.DEFAULT);
+        }
+    };
     private SecretKeySpec secretKeySpec;
     private final Cipher cipher;
+    private static final Charset UTF8 = Charset.forName("UTF-8");
+    private final Base64Encoder base64Encoder;
+
+    public interface Base64Encoder {
+        String encode(byte[] data);
+
+        byte[] decode(String encoded);
+    }
 
     @Override
     public Map<String, Object> readJson(Reader reader) throws IOException {
@@ -37,7 +61,7 @@ class TDJsonHandler implements KeenJsonHandler {
     private Map<String, Object> readJson(Reader reader, boolean withoutDecryption) throws IOException {
         if (withoutDecryption || secretKeySpec == null) {
             try {
-                return mapper.readValue(reader, MAP_TYPE);
+                return gson.fromJson(reader, MAP_TYPE);
             } catch (Exception e) {
                 Log.w(TAG, "This event can't be handled as a plain", e);
                 return null;
@@ -56,12 +80,12 @@ class TDJsonHandler implements KeenJsonHandler {
 
             String data = buf.toString();
             try {
-                byte[] decryptedBytes = decrypt(Base64.decode(data, Base64.DEFAULT));
-                return mapper.readValue(decryptedBytes, MAP_TYPE);
+                byte[] decryptedBytes = decrypt(base64Encoder.decode(data));
+                return gson.fromJson(new String(decryptedBytes, UTF8), MAP_TYPE);
             } catch (Exception e) {
                 Log.w(TAG, "Decryption failed. Trying to handle this event as a plain", e);
                 try {
-                    return mapper.readValue(data, MAP_TYPE);
+                    return gson.fromJson(data, MAP_TYPE);
                 } catch (Exception ee) {
                     Log.w(TAG, "This event can't be handled as a plain", ee);
                     return null;
@@ -85,20 +109,23 @@ class TDJsonHandler implements KeenJsonHandler {
 
     private void writeJson(Writer writer, Map<String, ?> value, boolean withoutEncryption) throws IOException {
         if (withoutEncryption || secretKeySpec == null) {
-            mapper.writeValue(writer, value);
+            gson.toJson(value, writer);
         }
         else {
             ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(byteArrayOutputStream));
-            mapper.writeValue(bufferedWriter, value);
+            gson.toJson(value, bufferedWriter);
+            bufferedWriter.close();
             try {
                 byte[] encryptedBytes = encrypt(byteArrayOutputStream.toByteArray());
-                writer.write(Base64.encodeToString(encryptedBytes, Base64.DEFAULT));
+                writer.write(base64Encoder.encode(encryptedBytes));
             } catch (Exception e) {
                 Log.w(TAG, "Encryption failed. Storing this event as a plain", e);
                 secretKeySpec = null;
+                gson.toJson(value, writer);
             }
         }
+        writer.close();
     }
 
     private byte[] encrypt(byte[] data) throws InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
@@ -120,7 +147,8 @@ class TDJsonHandler implements KeenJsonHandler {
         this(null);
     }
 
-    TDJsonHandler(String encryptionKeyword) {
+    // Exposing this API for testing
+    TDJsonHandler(String encryptionKeyword, Base64Encoder base64Encoder) {
         SecretKeySpec secretKeySpec = null;
         Cipher cipher = null;
         if (encryptionKeyword != null) {
@@ -135,29 +163,86 @@ class TDJsonHandler implements KeenJsonHandler {
         }
         this.secretKeySpec = secretKeySpec;
         this.cipher = cipher;
+        if (base64Encoder == null) {
+            this.base64Encoder = DEFAULT_BASE64_ENCODER;
+        }
+        else {
+            this.base64Encoder = base64Encoder;
+        }
 
-        mapper = new ObjectMapper();
-        mapper.setDateFormat(SRC_DATA_FORMAT);
+        gson = createGson();
+    }
+
+    TDJsonHandler(String encryptionKeyword) {
+        this(encryptionKeyword, null);
+    }
+
+    private Gson createGson() {
+        TypeAdapter<Number> doubleAdapter = new TypeAdapter<Number>()
+        {
+            private TypeAdapter<Number> delegate = TypeAdapters.DOUBLE;
+
+            @Override
+            public Number read(JsonReader in)
+                    throws IOException
+            {
+                return delegate.read(in);
+            }
+
+            @Override
+            public void write(JsonWriter out, Number value)
+                    throws IOException
+            {
+                if (value instanceof Double) {
+                    if (value.equals(Double.NaN) || value.equals(Double.POSITIVE_INFINITY) || value.equals(Double.NEGATIVE_INFINITY)) {
+                        out.value(value.toString());
+                        return;
+                    }
+                }
+                delegate.write(out, value);
+            }
+        };
+
+        TypeAdapter<Number> floatAdapter = new TypeAdapter<Number>()
+        {
+            private TypeAdapter<Number> delegate = TypeAdapters.FLOAT;
+
+            @Override
+            public Number read(JsonReader in)
+                    throws IOException
+            {
+                return delegate.read(in);
+            }
+
+            @Override
+            public void write(JsonWriter out, Number value)
+                    throws IOException
+            {
+                if (value instanceof Float) {
+                    if (value.equals(Float.NaN) || value.equals(Float.POSITIVE_INFINITY) || value.equals(Float.NEGATIVE_INFINITY)) {
+                        out.value(value.toString());
+                        return;
+                    }
+                }
+                delegate.write(out, value);
+            }
+        };
+
+        return new GsonBuilder().
+                registerTypeAdapterFactory(TypeAdapters.newFactory(float.class, Float.class, floatAdapter)).
+                registerTypeAdapterFactory(TypeAdapters.newFactory(double.class, Double.class, doubleAdapter)).
+                setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").
+                disableHtmlEscaping().
+                serializeNulls().create();
     }
 
     ///// PRIVATE CONSTANTS /////
 
-    private static final MapType MAP_TYPE =
-            TypeFactory.defaultInstance().constructMapType(Map.class, String.class, Object.class);
-
-    private static final SimpleDateFormat SRC_DATA_FORMAT;
-    private static final SimpleDateFormat DST_DATA_FORMAT;
-    static {
-        TimeZone timeZone = TimeZone.getTimeZone("GMT");
-        SRC_DATA_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
-        SRC_DATA_FORMAT.setTimeZone(timeZone);
-        DST_DATA_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
-        DST_DATA_FORMAT.setTimeZone(timeZone);
-    }
+    private static final Type MAP_TYPE = new TypeToken<Map<String, Object>>(){}.getType();
 
     ///// PRIVATE FIELDS /////
 
-    private final ObjectMapper mapper;
+    private final Gson gson;
 
 }
 
--- src/test/java/com/treasuredata/android/TDJsonHandlerTest.java
+++ src/test/java/com/treasuredata/android/TDJsonHandlerTest.java
@@ -1,46 +1,134 @@
 package com.treasuredata.android;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import junit.framework.TestCase;
+import org.apache.commons.codec.binary.Base64;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.io.Writer;
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TDJsonHandlerTest extends TestCase {
+    private static final String JSON_STR = "{\n" +
+                "    \"name\":\"komamitsu\",\n" +
+                "    \"age\":123,\n" +
+                "    \"keen\": {\n" +
+                "        \"timestamp\": \"2014-12-31T23:59:01.123+0000\"\n" +
+                "    },\n" +
+                "    \"#UUID\":\"2F1FCD4D-74A6-45EF-B9B0-CD82DE49BE69\"\n" +
+                "}";
     private TDJsonHandler jsonHandler;
-    private ObjectMapper objectMapper;
+    private Gson gson;
 
     public void setUp() throws Exception {
         super.setUp();
         jsonHandler = new TDJsonHandler();
-        objectMapper = new ObjectMapper();
+        gson = new Gson();
     }
 
     public void tearDown() throws Exception {
     }
 
     public void testReadJson() throws Exception {
+        Map<String, Object> result = jsonHandler.readJson(new StringReader(JSON_STR));
+        assertEquals("komamitsu", result.get("name"));
+        assertEquals(123.0, result.get("age"));
+        assertEquals("2014-12-31T23:59:01.123+0000", ((Map<String, Object>) result.get("keen")).get("timestamp"));
+        assertEquals("2F1FCD4D-74A6-45EF-B9B0-CD82DE49BE69", result.get("#UUID"));
     }
 
-    public void testWriteJson() throws Exception {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        Writer writer = new OutputStreamWriter(out);
+    private Map<String, ?> createExampleMap(Date now) {
+        Map<String, List<Map<String, Object>>> root = new HashMap<String, List<Map<String, Object>>>();
         Map<String, Object> value = new HashMap<String, Object>();
         value.put("name", "komamitsu");
         value.put("age", 123);
         Map<String, Object> keen = new HashMap<String, Object>();
         keen.put("timestamp", "2014-12-31T23:59:01.123+0000");
         value.put("keen", keen);
+        value.put("now", now);
+        value.put("f_nan", Float.NaN);
+        value.put("f_pos_inf", Float.POSITIVE_INFINITY);
+        value.put("f_neg_inf", Float.NEGATIVE_INFINITY);
+        value.put("d_nan", Double.NaN);
+        value.put("d_pos_inf", Double.POSITIVE_INFINITY);
+        value.put("d_neg_inf", Double.NEGATIVE_INFINITY);
+        value.put("#UUID", "2F1FCD4D-74A6-45EF-B9B0-CD82DE49BE69");
+        root.put("testdb.testtbl", Arrays.asList(value));
+
+        return root;
+    }
+
+    private void assertExampleMap(Date now, Map<String, Object> root) {
+        assertEquals(1, root.size());
+        Object rootValue = root.get("testdb.testtbl");
+        assertTrue(rootValue instanceof List);
+        List<Map<String, Object>> list = (List<Map<String, Object>>) rootValue;
+        assertEquals(1, list.size());
+        assertTrue(list.get(0) instanceof Map);
+
+        Map<String, Object> value = list.get(0);
+        assertEquals("komamitsu", value.get("name"));
+        assertEquals(123.0, value.get("age"));
+        assertEquals("2014-12-31T23:59:01.123+0000", ((Map<String, Object>) value.get("keen")).get("timestamp"));
+        assertEquals(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").format(now), value.get("now"));
+        assertEquals(String.valueOf(Float.NaN), value.get("f_nan"));
+        assertEquals(String.valueOf(Float.POSITIVE_INFINITY), value.get("f_pos_inf"));
+        assertEquals(String.valueOf(Float.NEGATIVE_INFINITY), value.get("f_neg_inf"));
+        assertEquals(String.valueOf(Double.NaN), value.get("d_nan"));
+        assertEquals(String.valueOf(Double.POSITIVE_INFINITY), value.get("d_pos_inf"));
+        assertEquals(String.valueOf(Double.NEGATIVE_INFINITY), value.get("d_neg_inf"));
+        assertEquals("2F1FCD4D-74A6-45EF-B9B0-CD82DE49BE69", value.get("#UUID"));
+    }
+
+    public void testWriteJson() throws Exception {
+        Date now = new Date();
+        Map<String, ?> value = createExampleMap(now);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Writer writer = new OutputStreamWriter(out);
         jsonHandler.writeJson(writer, value);
 
         String s = new String(out.toByteArray());
-        JsonNode jsonNode = objectMapper.readTree(s);
-        assertEquals("komamitsu", jsonNode.get("name").asText());
-        assertEquals(123, jsonNode.get("age").asInt());
-        assertEquals("2014-12-31T23:59:01.123+0000", jsonNode.get("keen").findValue("timestamp").asText());
+        Map<String, Object> result = gson.fromJson(s, new TypeToken<Map<String, Object>>() {}.getType());
+        assertExampleMap(now, result);
+    }
+
+    public void testReadWriteWithEncryption()
+            throws IOException
+    {
+        Date now = new Date();
+        Map<String, ?> value = createExampleMap(now);
+
+        TDJsonHandler encJsonHandler = new TDJsonHandler("hello, world", new TDJsonHandler.Base64Encoder() {
+            private final Charset UTF8 = Charset.forName("UTF-8");
+            @Override
+            public String encode(byte[] data)
+            {
+                return new String(Base64.encodeBase64(data), UTF8);
+            }
+
+            @Override
+            public byte[] decode(String encoded)
+            {
+                return Base64.decodeBase64(encoded.getBytes(UTF8));
+            }
+        });
+
+        StringWriter writer = new StringWriter();
+        encJsonHandler.writeJson(writer, value);
+
+        Map<String, Object> result = encJsonHandler.readJson(new StringReader(writer.toString()));
+        assertExampleMap(now, result);
     }
 }
