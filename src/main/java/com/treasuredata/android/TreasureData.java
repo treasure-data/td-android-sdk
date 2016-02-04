25f9ebb78a4578607225188b9d43005bd4a92672
Mitsunori Komatsu <komamitsu@gmail.com>
2016-02-05 01:09:22 +0900

    Bump version 0.1.10

--- src/main/java/com/treasuredata/android/TreasureData.java
+++ src/main/java/com/treasuredata/android/TreasureData.java
@@ -16,7 +16,7 @@ import java.util.regex.Pattern;
 
 public class TreasureData {
     private static final String TAG = TreasureData.class.getSimpleName();
-    private static final String VERSION = "0.1.9";
+    private static final String VERSION = "0.1.10";
     private static final String LABEL_ADD_EVENT = "addEvent";
     private static final String LABEL_UPLOAD_EVENTS = "uploadEvents";
     private static final Pattern DATABASE_NAME_PATTERN = Pattern.compile("^[0-9a-z_]{3,255}$");
