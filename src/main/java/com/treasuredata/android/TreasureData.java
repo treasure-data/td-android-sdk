cb29db73954ec67424df23032ab11816f7f9d2b9
vinhvd@gmail.com <vinhvd@gmail.com>
2018-02-13 09:58:30 +0700

    Add Android api level 14 condition check for application lifecycle callback

--- src/main/java/com/treasuredata/android/TreasureData.java
+++ src/main/java/com/treasuredata/android/TreasureData.java
@@ -233,49 +233,50 @@ public class TreasureData {
         this.appVersionNumber = appVersionNumber;
 
         this.client = client;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            final Application application = (Application) applicationContext;
+            application.registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
+                final AtomicBoolean trackedApplicationLifecycleEvents = new AtomicBoolean(false);
 
-        final Application application = (Application)applicationContext;
-        application.registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
-            final AtomicBoolean trackedApplicationLifecycleEvents = new AtomicBoolean(false);
-
-            @Override
-            public void onActivityCreated(Activity activity, Bundle bundle) {
-                if (!trackedApplicationLifecycleEvents.getAndSet(true)
-                        && autoTrackApplicationLifecycleEvents) {
-                    trackApplicationLifecycleEvents();
+                @Override
+                public void onActivityCreated(Activity activity, Bundle bundle) {
+                    if (!trackedApplicationLifecycleEvents.getAndSet(true)
+                            && autoTrackApplicationLifecycleEvents) {
+                        trackApplicationLifecycleEvents();
+                    }
                 }
-            }
 
-            @Override
-            public void onActivityStarted(Activity activity) {
+                @Override
+                public void onActivityStarted(Activity activity) {
 
-            }
+                }
 
-            @Override
-            public void onActivityResumed(Activity activity) {
+                @Override
+                public void onActivityResumed(Activity activity) {
 
-            }
+                }
 
-            @Override
-            public void onActivityPaused(Activity activity) {
+                @Override
+                public void onActivityPaused(Activity activity) {
 
-            }
+                }
 
-            @Override
-            public void onActivityStopped(Activity activity) {
+                @Override
+                public void onActivityStopped(Activity activity) {
 
-            }
+                }
 
-            @Override
-            public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
+                @Override
+                public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
 
-            }
+                }
 
-            @Override
-            public void onActivityDestroyed(Activity activity) {
+                @Override
+                public void onActivityDestroyed(Activity activity) {
 
-            }
-        });
+                }
+            });
+        }
     }
 
     private void trackApplicationLifecycleEvents() {
