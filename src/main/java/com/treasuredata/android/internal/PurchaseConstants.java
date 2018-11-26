5d40a7b6443c038a71415c8451fb9db9634b5622
Vinh Vo <vinhvd@gmail.com>
2018-11-26 09:14:05 +0700

    Fix ci error

---
+++ src/main/java/com/treasuredata/android/internal/PurchaseConstants.java
@@ -0,0 +1,29 @@
+package com.treasuredata.android.internal;
+
+public class PurchaseConstants {
+    // Purchase types
+    public static final String SUBSCRIPTION = "subs";
+    public static final String INAPP = "inapp";
+    public static final String EVENT_KEY = "td_android_event";
+    public static final String IAP_EVENT_NAME = "TD_ANDROID_IN_APP_PURCHASE";
+    public static final String IAP_PRODUCT_ID = "td_iap_product_id";
+    public static final String IAP_ORDER_ID = "td_iap_order_id";
+    public static final String IAP_PRODUCT_PRICE = "td_iap_product_price";
+    public static final String IAP_PRODUCT_PRICE_AMOUNT_MICROS= "td_iap_product_price_amount_micros";
+    public static final String IAP_PRODUCT_CURRENCY = "td_iap_product_currency";
+    public static final String IAP_QUANTITY  = "td_iap_quantity";
+    public static final String IAP_PURCHASE_TIME = "td_iap_purchase_time";
+    public static final String IAP_PURCHASE_TOKEN = "td_iap_purchase_token";
+    public static final String IAP_PURCHASE_STATE = "td_iap_purchase_state";
+    public static final String IAP_PURCHASE_DEVELOPER_PAYLOAD = "td_iap_purchase_developer_payload";
+    public static final String IAP_PRODUCT_TYPE = "td_iap_product_type";
+    public static final String IAP_PRODUCT_TITLE = "td_iap_product_title";
+    public static final String IAP_PRODUCT_DESCRIPTION = "td_iap_product_description";
+    public static final String IAP_PACKAGE_NAME = "td_iap_package_name";
+    public static final String IAP_SUBSCRIPTION_AUTORENEWING = "td_iap_subs_auto_renewing";
+    public static final String IAP_SUBSCRIPTION_PERIOD = "td_iap_subs_period";
+    public static final String IAP_FREE_TRIAL_PERIOD = "td_iap_free_trial_period";
+    public static final String IAP_INTRO_PRICE_AMOUNT_MICROS = "td_iap_intro_price_amount_micros";
+    public static final String IAP_INTRO_PRICE_PERIOD = "td_iap_intro_price_period";
+    public static final String IAP_INTRO_PRICE_CYCLES = "td_iap_intro_price_cycles";
+}
