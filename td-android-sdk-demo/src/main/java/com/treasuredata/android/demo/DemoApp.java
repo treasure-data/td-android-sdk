61247423a000a077d15fe5b0a54839e9bb335057
huylenq <huy.lenq@gmail.com>
2019-06-10 18:14:05 +0700

    Restructure to use Gradle

--- td-android-sdk-demo/src/main/java/com/treasuredata/android/demo/DemoApp.java
+++ td-android-sdk-demo/src/main/java/com/treasuredata/android/demo/DemoApp.java
@@ -1,8 +1,14 @@
 package com.treasuredata.android.demo;
 
 import android.app.Application;
+import android.os.Looper;
 
 import com.treasuredata.android.TreasureData;
+import com.treasuredata.android.cdp.CDPClient;
+import com.treasuredata.android.cdp.FetchUserSegmentCallback;
+
+import java.io.IOException;
+import java.util.Map;
 
 /**
  * Created by vinhvd on 2/6/18.
@@ -26,5 +32,22 @@ public class DemoApp extends Application {
         TreasureData.sharedInstance().enableAutoAppendRecordUUID();
         TreasureData.sharedInstance().setDefaultDatabase("test_db");
         TreasureData.sharedInstance().setDefaultTable("test_tbl");
+
+        try {
+            new CDPClient().justTrying(Looper.myLooper(), new FetchUserSegmentCallback() {
+                @Override
+                public void onSuccess(Map<String, ?> map) {
+                    System.out.println(">> Success");
+                }
+
+                @Override
+                public void onError(String s, String s1) {
+                    System.out.println(">> Error");
+                }
+            });
+        } catch (IOException e) {
+            System.out.println(">> IO Error");
+            e.printStackTrace();
+        }
     }
 }
