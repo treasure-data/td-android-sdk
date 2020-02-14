95ac106b71d60acbfe38b52efb0681f0aa4aef90
Tung Vu <tung.vu@treasure-data.com>
2020-02-14 19:23:02 +0700

    Use Play Referrer API instead of Install Broadcast Intent

--- example/src/main/java/com/treasuredata/android/demo/DemoApp.java
+++ example/src/main/java/com/treasuredata/android/demo/DemoApp.java
@@ -1,16 +1,30 @@
 package com.treasuredata.android.demo;
 
 import android.app.Application;
+import android.os.RemoteException;
+
+import com.android.installreferrer.api.InstallReferrerClient;
+import com.android.installreferrer.api.InstallReferrerStateListener;
+import com.android.installreferrer.api.ReferrerDetails;
 import com.treasuredata.android.TreasureData;
+
+import java.util.HashMap;
 
 /**
  * Created by vinhvd on 2/6/18.
  */
 
 public class DemoApp extends Application {
+    InstallReferrerClient referrerClient;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        setupTreasureData();
+        setupInstallReferrer();
+    }
+
+    private void setupTreasureData() {
         // TreasureData.initializeApiEndpoint("https://specify-other-endpoint-if-needed.com");
         TreasureData.enableLogging();
         TreasureData.initializeEncryptionKey("hello world");
@@ -27,4 +41,52 @@ public class DemoApp extends Application {
         TreasureData.sharedInstance().setDefaultDatabase("test_db");
         TreasureData.sharedInstance().setDefaultTable("test_tbl");
     }
+
+    private void setupInstallReferrer() {
+        referrerClient = InstallReferrerClient.newBuilder(this).build();
+        referrerClient.startConnection(new InstallReferrerStateListener() {
+            @Override
+            public void onInstallReferrerSetupFinished(int responseCode) {
+                switch (responseCode) {
+                    case InstallReferrerClient.InstallReferrerResponse.OK:
+                        // Connection established.
+                        try {
+                            ReferrerDetails response = referrerClient.getInstallReferrer();
+                            addReferrerEvent(response);
+                        } catch (RemoteException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case InstallReferrerClient.InstallReferrerResponse.FEATURE_NOT_SUPPORTED:
+                        // API not available on the current Play Store app.
+                        break;
+                    case InstallReferrerClient.InstallReferrerResponse.SERVICE_UNAVAILABLE:
+                        // Connection couldn't be established.
+                        break;
+                }
+            }
+
+            @Override
+            public void onInstallReferrerServiceDisconnected() {
+                // Try to restart the connection on the next request to
+                // Google Play by calling the startConnection() method.
+            }
+        });
+    }
+
+    private void addReferrerEvent(ReferrerDetails referrer) {
+        String referrerUrl = referrer.getInstallReferrer();
+        long referrerClickTime = referrer.getReferrerClickTimestampSeconds();
+        long appInstallTime = referrer.getInstallBeginTimestampSeconds();
+        boolean instantExperienceLaunched = referrer.getGooglePlayInstantParam();
+        HashMap<String, Object> eventRecord = new HashMap<String, Object>();
+        eventRecord.put("type", "install_referrer");
+        eventRecord.put("url", referrerUrl);
+        eventRecord.put("referrerClickTime", referrerClickTime);
+        eventRecord.put("appInstallTime", appInstallTime);
+        eventRecord.put("instantExperienceLaunched", instantExperienceLaunched);
+        TreasureData.sharedInstance().addEvent("test_db", "demo_tbl", eventRecord);
+        TreasureData.sharedInstance().uploadEvents();
+    }
+
 }
